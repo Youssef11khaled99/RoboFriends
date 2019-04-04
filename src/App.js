5c179e5b1dab5b816e205ea2f19a8d6466b7a516
Youssef11khaled99 <uossefkhaled99@gmail.com>
2019-04-05 00:47:10 +0200

    Adding loading screen

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
-import {robots} from './robots';
+//import {robots} from './robots';
 import SearchBox from './SearchBox';
 import './App.css';
+import './LoadingScreen.css';
 
 class App extends Component {
     constructor(){
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchValue: ''
         }
     }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json() )
+        .then (users => this.setState({robots : users}));
+    }
+
     onSearchChange = (event) => {
         this.setState({searchValue : event.target.value})
     }
@@ -20,14 +28,23 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter((robot) => {
             return robot.name.toLowerCase().includes(this.state.searchValue.toLowerCase())
         })
-    
-        return(
-            <div className='tc'>
-                <h1 className="f1"> Robofriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots}/>
-            </div>
-        );
+        if (this.state.robots.length === 0) {
+            return (
+                <div>
+                    <div class="back"><h1 className='tc'>Loading ....</h1></div>
+                    <div class="cube"></div>
+                </div>
+            );
+        }else {
+            return(
+                <div className='tc'>
+                    <h1 className="f1"> Robofriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <CardList robots={filteredRobots}/>
+                </div>
+            );
+
+        }
     }
 }
 
