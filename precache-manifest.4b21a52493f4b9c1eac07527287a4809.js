f5cdfa5b3c650534c694b8f45db66bf8a2e6de1d
Youssef11khaled99 <uossefkhaled99@gmail.com>
2019-05-03 15:15:08 +0200

    Updates

---
+++ precache-manifest.4b21a52493f4b9c1eac07527287a4809.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "d48d68f29921c5c588da",
+    "url": "/RoboFriends/static/js/runtime~main.19bee101.js"
+  },
+  {
+    "revision": "10c28fe62f3193dfdd63",
+    "url": "/RoboFriends/static/js/main.c777c28a.chunk.js"
+  },
+  {
+    "revision": "aa092b2fd96b909956dc",
+    "url": "/RoboFriends/static/js/2.0baed833.chunk.js"
+  },
+  {
+    "revision": "10c28fe62f3193dfdd63",
+    "url": "/RoboFriends/static/css/main.16cc3c03.chunk.css"
+  },
+  {
+    "revision": "aa092b2fd96b909956dc",
+    "url": "/RoboFriends/static/css/2.b44541a5.chunk.css"
+  },
+  {
+    "revision": "7115301d49a80f87b05e082034185335",
+    "url": "/RoboFriends/index.html"
+  }
+];
